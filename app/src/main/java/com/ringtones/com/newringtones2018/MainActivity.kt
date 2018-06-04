1d71a1a2488bb20d9bc70edfc3ab1a40a8326e27
sachin1kumar <sachinkmr375@gmail.com>
2018-06-04 22:48:19 +0530

    back up with tones.

--- app/src/main/java/com/ringtones/com/newringtones2018/MainActivity.kt
+++ app/src/main/java/com/ringtones/com/newringtones2018/MainActivity.kt
@@ -18,9 +18,13 @@ class MainActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
 
     //Add a list items in String
-    val listContent = arrayOf("apple_ring", "astronomia","nice_ringtone")
+    val listContent = arrayOf("Base gente remix","Shape of you","Apple ringtone","Apple ringtone extended","Base william voodoo", "Astronomia","Nice ringtone","Dad is calling","Titanic flute","Fade"
+    ,"I miss you","Iphone 8 plus ringtone","Cock ringtone","Iphone 8 plus sms","Best flute ringtone","Classic phone","Nokia","You got text message","Water ring","Ting tong","Tweet ringtone","S8 charger connected","Warrior pixel2","Zooh",
+            "Xiaomi notification","Break","Tear down","Fall down","Squeeze","Alert","Unlock","Bee bom")
     //Add a resource of music files in Array
-    val resID = intArrayOf(R.raw.apple_ring, R.raw.astronomia,R.raw.nice_ringtone)
+    val resID = intArrayOf(R.raw.mi_gente_bassremix,R.raw.shape_of_you,R.raw.apple_ring,R.raw.a_2,R.raw.willy_william_voodoo, R.raw.astronomia,R.raw.nice_ringtone,R.raw.dad_is_calling,R.raw.titanic_flute,R.raw.fade,
+            R.raw.i_miss_you,R.raw.iphone_8_plus_ring,R.raw.a_22,R.raw.iphone_sms_original,R.raw.zedge_best_flute,R.raw.aa_4,R.raw.aa_5,R.raw.c_17,R.raw.a_66,R.raw.b_1,R.raw.tweet_ringtone,R.raw.s8_charge_connected,
+            R.raw.warriors_pixel2,R.raw.s_16,R.raw.s_15,R.raw.s_13,R.raw.s_10,R.raw.s_7,R.raw.s_6,R.raw.s_2,R.raw.s_1,R.raw.c_8)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +51,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setHeadingOnTab() {
-        mTabLayout!!.addTab(mTabLayout!!.newTab().setText("Today's Latest Tech News"))
+        mTabLayout!!.addTab(mTabLayout!!.newTab().setText("Play and set your tone"))
     }
 
 
